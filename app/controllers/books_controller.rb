be091310fc31c7c8eb952c026e467f2fe24e44a5
mikeyroush <mikey.roush97@gmail.com>
2021-01-28 13:43:48 -0600

    added CRUD actions

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,55 @@
+class BooksController < ApplicationController
+  
+  # Create
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to books_path
+    else
+      render('new')
+    end
+  end
+  
+  # Read
+  def index
+    @books = Book.order('title ASC')
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  # Update
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+    else
+      render('edit')
+    end
+  end
+
+  # Delete
+  def delete
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :author, :genre, :price, :published_date)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+
+  # root
+  root 'books#index'
+
+  resources :books do
+    member do
+      get :delete
+    end
+  end
+
+  # get 'books/index'
+  # get 'books/show'
+  # get 'books/new'
+  # get 'books/edit'
+  # get 'books/delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
